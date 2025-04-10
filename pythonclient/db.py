0ae91df3ee84e2e048ad8eb7ee20b4acc396ed64
Pascal Véron <veron@univ-tln.fr>
2025-04-10 13:18:42 +0200

    Mise à jour client Python

--- pythonclient/db.py
+++ pythonclient/db.py
@@ -9,18 +9,11 @@ from Cryptodome.Hash import SHA3_256,SHA512
 from Cryptodome.Random import get_random_bytes
 
 def afficher_contenu_base_de_donnees(passwd,nom_base_de_donnees):
-    # Se connecter à la base de données
     pwd=bytes(passwd.encode('utf-8'))
     connexion = sqlite3.connect(nom_base_de_donnees)
     curseur = connexion.cursor()
-
-    # Exécuter une requête pour sélectionner toutes les données de la table
     curseur.execute("SELECT * FROM PASSWORDS")
-
-    # Récupérer tous les résultats
     resultats = curseur.fetchall()
-
-    # Afficher les résultats
     for resultat in resultats[1:]:
         nom_organisme = resultat[0]
         chiffre = resultat[1]
@@ -35,22 +28,14 @@ def afficher_contenu_base_de_donnees(passwd,nom_base_de_donnees):
         #aescbciv =  AES.new(cle,AES.MODE_CBC,iv=ivnull)
         aescbciv =  AES.new(cle,AES.MODE_ECB)
         ivclair = aescbciv.decrypt(ivchiffre)
-        #print("ivclair",cryptogramme)
-        #ivclair = unpad(ivclair,8)
         aescbc =  AES.new(cle,AES.MODE_CBC,iv=ivclair)
         message = aescbc.decrypt(cryptogramme)
         print(nom_organisme)
-        #print(ivclair)
         print(unpad(message,16).decode('utf-8'))
         print("")
-
-    # Fermer la connexion 
     connexion.close()
 
-# Remplacez "nom_de_la_base_de_donnees.db" par le nom de votre fichier de base de données SQLite
 nom_de_la_base_de_donnees = "passwords.db"
 mdp = input("Entrez mot de passe: ")
-
-# Appelez la fonction pour afficher le contenu de la base de données
 afficher_contenu_base_de_donnees(mdp,nom_de_la_base_de_donnees)
 
